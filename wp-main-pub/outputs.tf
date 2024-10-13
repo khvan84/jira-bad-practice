4444a6da19424aa68960a6457236016c76fd5688
Alexander Khvan <alexander.khv@edu.312school.com>
2024-10-13 05:18:27 -0500

    Delete wp-main-pub directory

--- wp-main-pub/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "vpc_id" {
-  value = aws_vpc.wp-vpc
-}
-
-output "db-wp-sg-id" {
-  value = aws_security_group.allow_multiports_wordpress_sg
-}
-
--- wp-main-pub/wordpress_igw.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_internet_gateway" "wordpress_igw" {
-  vpc_id = aws_vpc.wp-vpc.id
-
-  tags = {
-    Name = var.wordpress_igw_name
-  }
-}
